c36edf0f876efdc34f1fef00f635d04e626287f8
Jesper van den Ende <jespertheend@gmail.com>
2023-04-16 00:46:07 +0200

    feat: Better layout and add homepage logo

--- site/components/Header.tsx
+++ site/components/Header.tsx
@@ -3,10 +3,12 @@ import { DangerousSvg } from "./DangerousSvg.tsx";
 import { getStaticFileText } from "../util/getStaticFile.ts";
 import { isRelativeUrl } from "../util/isRelativeUrl.ts";
 
+const rendaSvg = await getStaticFileText("renda.svg");
 const githubSvg = await getStaticFileText("github.svg");
 
-function HeaderLink({ href, children, ariaLabel }: {
+function HeaderLink({ href, classes, children, ariaLabel }: {
 	href: string;
+	classes?: string;
 	children: JSX.Element | string;
 	ariaLabel?: string;
 }) {
@@ -15,18 +17,27 @@ function HeaderLink({ href, children, ariaLabel }: {
 		attributes.target = "_blank";
 		attributes.rel = "noopener noreferrer";
 	}
-	return <a href={href} {...attributes} aria-label={ariaLabel}>{children}</a>;
+	return <a href={href} {...attributes} class={classes} aria-label={ariaLabel}>{children}</a>;
 }
 
 export function Header() {
 	return (
 		<header>
-			<HeaderLink href="/">Home</HeaderLink>
-			<HeaderLink href="/manual">Manual</HeaderLink>
-			<HeaderLink href="https://renda.studio">Studio</HeaderLink>
-			<HeaderLink href="https://github.com/rendajs" ariaLabel="GitHub">
-				<DangerousSvg svg={githubSvg} />
-			</HeaderLink>
+			<div class="header-content">
+				<HeaderLink href="/" ariaLabel="Renda Home" classes="home-link">
+					<>
+						<DangerousSvg svg={rendaSvg} />
+						Renda
+					</>
+				</HeaderLink>
+				<nav>
+					<HeaderLink href="/manual">Manual</HeaderLink>
+					<HeaderLink href="https://renda.studio">Studio</HeaderLink>
+					<HeaderLink href="https://github.com/rendajs" ariaLabel="GitHub">
+						<DangerousSvg svg={githubSvg} />
+					</HeaderLink>
+				</nav>
+			</div>
 		</header>
 	);
 }
