ee5bb3082cb0c61c29f4e07facf67bd746ab68d8
Jesper van den Ende <jespertheend@gmail.com>
2024-03-24 16:10:18 +0100

    feat: Cache svg requests

--- site/routes/static.ts
+++ site/routes/static.ts
@@ -6,10 +6,18 @@ const staticRoot = new URL("../static", import.meta.url).pathname;
 export const staticHandler: RouteHandler = {
 	pattern: new URLPattern({ pathname: "/static/*" }),
 	async handler(request) {
-		return await serveDir(request, {
+		const response = await serveDir(request, {
 			quiet: true,
 			fsRoot: staticRoot,
 			urlRoot: "static/",
 		});
+		const contentType = response.headers.get("Content-Type");
+		// For now we'll only cache svg requests since we might want to make changes to .js and .css files.
+		// In production, css and js files are inlined in the html, so these resources aren't requested anyway.
+		if (contentType?.startsWith("image/svg")) {
+			const thirtyDays = 60 * 60 * 24 * 30;
+			response.headers.set("Cache-Control", `public, max-age=${thirtyDays}, stale-while-revalidate=${thirtyDays}`);
+		}
+		return response;
 	},
 };
